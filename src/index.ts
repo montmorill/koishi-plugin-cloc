551b52112315cc044b08a24566338a36267551b6
montmorill <montmorill@outlook.com>
2026-07-10 01:44:44 +0800

    refactor: update Config interface and enhance command functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { Context, Schema } from 'koishi'
+import { spawn } from 'node:child_process'
 
 export const name = 'cloc'
 
-export interface Config {}
+export interface Config {
+  excludeDirs: string[]
+  workingDir: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  excludeDirs: Schema.array(Schema.string()).default(['node_modules', 'dist', 'satori', 'upstream']),
+  workingDir: Schema.string().default('external'),
+})
 
 export function apply(ctx: Context, config: Config) {
-  // write your plugin here
+  ctx.command('cloc', '统计代码行数').action(async () => {
+    const proc = await spawn('wsl', [
+      'cloc',
+      '--exclude-dir', config.excludeDirs.join(','),
+      '--include_ext', 'ts,yml',
+      config.workingDir, '--json'
+    ], { stdio: 'pipe' })
+
+    let buffer: string = ''
+    for await (const chunk of proc.stdout)
+      buffer += chunk
+    return JSON.parse(buffer).SUM.code
+  })
 }
