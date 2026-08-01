a3c46764e030af1566ae64264270d155cb8c7e7e
montmorill <montmorill@outlook.com>
2026-08-01 22:22:31 +0800

    refactor: update Config schema to remove includeExts and enhance cloc function for better output handling

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,69 @@ import {} from '@koishijs/plugin-hmr'
 export const name = 'cloc'
 
 export interface Config {
-  excludeDirs: string[]
-  includeExts: string[]
   workingDir: string
+  excludeDirs: string[]
+  extraArgs: string[]
 }
 
 export const Config: Schema<Config> = Schema.object({
-  excludeDirs: Schema.array(Schema.string()).default(['node_modules', 'dist', 'lib', 'satori']),
-  includeExts: Schema.array(Schema.string()).default(['ts', 'yml']),
-  workingDir: Schema.string().default('external'),
+  workingDir: Schema.string().role('folder').default('external'),
+  excludeDirs: Schema.array(Schema.string()).default([
+    'node_modules',
+    'dist', 'lib',
+    'satori', 'database'
+  ]),
+  extraArgs: Schema.array(Schema.string()).experimental().default([]),
 })
 
-async function countLoc(config: Config): Promise<number> {
-  const proc = await spawn('wsl', [
-    'cloc',
+const types = ['nFiles', 'blank', 'comment', 'code'] as const
+type ClocOutput = Record<"SUM" | string, Record<typeof types[number], number>>
+
+async function cloc(config: Config): Promise<ClocOutput> {
+  let command = 'cloc'
+  const args = [
     '--exclude-dir', config.excludeDirs.join(','),
-    '--include-ext', config.includeExts.join(','),
-    config.workingDir, '--json'
-  ], { stdio: 'pipe' })
+    '--quiet',
+    '--json',
+    config.workingDir,
+    ...config.extraArgs,
+  ]
+
+  if (process.platform === 'win32') {
+    args.unshift(command)
+    command = 'wsl'
+  }
+
+  const proc = await spawn(command, args, { stdio: 'pipe' })
 
   let buffer: string = ''
   for await (const chunk of proc.stdout)
     buffer += chunk
   const data = JSON.parse(buffer)
+
   delete data.header
-  return data.SUM.code
+  return data
 }
 
 export async function apply(ctx: Context, config: Config) {
-  let loc = await countLoc(config)
-  ctx.on('hmr/reload', () => void countLoc(config).then((res) => loc = res))
-  ctx.command('cloc', '统计代码行数').action(() => String(loc))
+  let loc = await cloc(config)
+  ctx.on('hmr/reload', () => void cloc(config).then((res) => loc = res))
+
+  const command = ctx.command('cloc')
+    .option('lang', '-l <lang>')
+    .option('type', '-t <type>', { type: types })
+    .option('type', '--n-files', { value: 'nFiles' })
+    .option('type', '--blank', { value: 'blank' })
+    .option('type', '--comment', { value: 'comment' })
+    .option('type', '--code', { value: 'code' })
+    .action(({ session, options }) => {
+      const type: typeof types[number] = options?.type || 'code'
+      const lang = options?.lang || 'SUM'
+      if (lang in loc)
+        return String(loc[lang][type])
+      return session?.text('.lang-not-found')
+    })
+
+  command.subcommand('.list')
+    .action(() => Object.keys(loc).join('\n'))
 }
